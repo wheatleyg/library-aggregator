44bbaf2646d441601b077b486e341c66db008c7c
Graceson <gwheatley@ftstudent.org>
2025-12-19 08:30:27 -0500

    feat: added feature bar
    Added navbar as seen in Figma design. Researching more into Vue to find out how to make components.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,26 +5,84 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/fchs_logo_cirlce.png"/>
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <ul class="nav-list">
+      <li><h1>Location</h1></li>
+        <li><h1>About</h1></li>
+        <li><h1>Help</h1></li>
+
+      </ul>
+      <h2 style="margin-left: 40px; font-style: italic; font-size: 16px"> Admin</h2>
+      <input style="margin-left:65px; height:65%; font-size: 18px" type="text" value="Search. . . ">
+
     </div>
   </header>
 
+
   <main>
     <TheWelcome />
-  </main>
+          </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+  background-color: white;
+  width: 100%;
+  height: 90px;
+  display: flex;
+  align-items: center;
+}
+.wrapper {
+  background-color: #92CBFF;
+  width: 100%;
+  margin-left:-20px;
+  padding-left: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+
+
+}
+.header-content {
+  margin-left: 20px;
+}
+.nav-list {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding: 0;
 }
 
+.nav-list li {
+  display: flex;
+  align-items: center;
+  margin: 0 20px 0 0;
+
+}
+
+.nav-list li::after {
+  content: "";
+  width: 18px;
+  height: 18px;
+  background-image: url("./assets/header_icon.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  margin-right: 2px;
+  margin-left: 2px;
+  margin-top: 8px;
+
+}
+
+
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  float: left;
+  height: 100%;
+  position: relative;
+  z-index: 1;
 }
 
 @media (min-width: 1024px) {
@@ -32,6 +90,7 @@ header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
+
   }
 
   .logo {
