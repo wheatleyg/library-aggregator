abf6f2e9aa0c66a04ddbfef89e486765649b790a
Graceson <gwheatley@ftstudent.org>
2026-02-12 08:45:29 -0500

    Add AppBar image, search, and sidebar items
    Add geometric-blue.jpg asset and enhance UI: update AppBar to use the image background, move logo into a prepend slot, constrain toolbar title, add a compact search field and spacer. Update Sidebar to show a generic "User" item, add a dense nav list with Location/About/Help items, and add empty <script setup> blocks in both components.

--- frontend/src/components/AppBar.vue
+++ frontend/src/components/AppBar.vue
@@ -1,8 +1,31 @@
+<script setup>
+</script>
 <template>
-  <v-app-bar app :elevation="5">
-    <v-img src="/fchs_logo_cirlce.png"></v-img>
-    <v-toolbar-title>
+  <v-app-bar app :elevation="5" class="pr-4" image="/geometric-blue.jpg" color="dark-">
+
+    <template #prepend>
+      <v-img src="/fchs_logo_cirlce.png" contain width="50" height="50"></v-img>
+
+    </template>
+    <v-toolbar-title style="min-width:200px">
       Franklin Central High School
     </v-toolbar-title>
+    <v-spacer />
+    <div style="width:400px">
+      <v-text-field
+          density="compact"
+          label="Search"
+          single-line
+          style="width: 400px;"
+          hide-details="auto"
+          class="mr-4"
+          variant="solo"
+      ></v-text-field>
+    </div>
+
+
+
+
+
     </v-app-bar>
 </template>
